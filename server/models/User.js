ae293333005d8efa7e2045329a4d41dab0ed980f
Bryce Berwald <bryce@berwald.io>
2023-12-19 18:12:02 -0800

    Cleaned up User.js mongo db schema file on server-side.

--- server/models/User.js
+++ server/models/User.js
@@ -1,29 +1,10 @@
-// const mongoose = require('mongoose');
-// const { Schema } = mongoose;
-
-// const UserSchema = new Schema({
-//     username: {
-//         type: String,
-//         required: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//     },
-//     password:{
-//         type: String,
-//         required: true,
-//     },
-// });
-
-// module.exports = mongoose.model('User', UserSchema);
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const CharacterSchema = new Schema({
     x: { type: Number, required: true },
     y: { type: Number, required: true },
-    frame: { type: Number, required: true }, // Assuming 'facing' can be a string like 'north', 'south', 'east', 'west'
+    frame: { type: Number, required: true },
     level: { type: Number, required: true },
 });
 
@@ -40,7 +21,7 @@ const UserSchema = new Schema({
         type: String,
         required: true,
     },
-    character: CharacterSchema, // Embedding the Character schema inside the User schema
+    character: CharacterSchema,
 });
 
 module.exports = mongoose.model('User', UserSchema);
