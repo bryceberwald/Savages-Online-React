b39d5a3cc3accdd0defa493f74dc4ba7d8421c41
Bryce Berwald <bryce@berwald.io>
2023-12-19 17:32:45 -0800

    Cleaned up server-side socket code.

--- server/socket.js
+++ server/socket.js
@@ -4,7 +4,6 @@ const { v4: uuidv4 } = require('uuid');
 
 const players = {};
 const sockets = {};
-
 const usernames = {};
 
 function initializeSocket(server, corsOptions) {
@@ -17,30 +16,31 @@ function initializeSocket(server, corsOptions) {
     // Generate a unique player ID for the connected player
     const playerId = uuidv4();
 
-    // Display a message to ensure connection
+    // Display a message to console when a player has connected to the socket.
     console.log(`Player ${playerId} connected`);
 
-    // Store the player ID in the `players` object
+    // Assign players {} object initial variable values.
     players[playerId] = { x: 0, y: 0, frame: 0 };
     sockets[playerId] = socket;
 
-    // Send the player ID to the connected client
+    // Send the playerId to the game client.
     socket.emit("playerId", playerId);
 
+    // Listen to get the players username to store to the usernames {} object.
     socket.on("getUsername", user => {
       usernames[playerId] = user;
-      console.log(usernames)
+      console.log(usernames);
     });
 
     // Listen for a new chat message to be emitted from the player.
     socket.on("chatMessage", (msg) => {
-      Object.values(sockets).forEach(sock => sock.emit("displayChatMessage", msg, playerId))
+      Object.values(sockets).forEach(sock => sock.emit("displayChatMessage", msg, playerId));
     });
 
     // Listen for a change in the players position
-    socket.on("playerPosition", async (x, y, frame, username) => {
+    socket.on("playerPosition", async (x, y, frame) => {
       try {
-          // Update players coordinates in the players objects {x, y}
+          // Update players location in the players objects {x, y, frame}
           players[playerId].x = x;
           players[playerId].y = y;
           players[playerId].frame = frame;
@@ -49,57 +49,47 @@ function initializeSocket(server, corsOptions) {
           socket.emit("updatePlayerPositions", players);
       } catch (error) {
           console.error("Error updating player position:", error);
-      }
+      };
     });
-
-    socket.on("savePlayerData", username => {
-      console.log("attempting to save player data...")
-      // const user = await User.findOne({username});
-
-      // if (user) {
-      //     user.character.x = x;
-      //     user.character.y = y;
-      //     user.character.frame = frame;
-      //     await user.save();
-      //     console.log("save completed.")
-      // };
-      for(const u in usernames){
-        if(usernames[u] === playerId){
-
-        }
-      }
-    });
-
 
     socket.on("disconnect", async () => {
+      
       // Display a message to ensure disconnection
       console.log(`Player ${playerId} disconnected`);
 
+      // Loop through all of the 
       for(const u in usernames){
         if(u === playerId){
-          // console log the username of the player disconnecting.
           console.log(usernames[u]);
-
           const username = usernames[u];
 
-          // save data to mongo database schema.
+          // Find the player in database searching by the username.
           const user = await User.findOne({username});
 
+          // Check if username was found in database, saving information if so.
           if (user) {
               user.character.x = players[playerId].x;
               user.character.y = players[playerId].y;
               user.character.frame = players[playerId].frame;
               await user.save();
-              console.log("save completed.")
+              console.log("save completed.");
           };
-        }
-      }
+
+        };
+      };
+
+      // Remove the disconnected player from the usernames object {}
+      delete usernames[playerId];
+
+      // Remove the disconnected player from the sockets object {}
+      delete sockets[playerId];
       
       // Remove the disconnected player from the players object {}
       delete players[playerId];
 
       // Broadcast the updated player positions to all connected clients
       socket.emit("updatePlayerPositions", players);
+
     });
       
   });
