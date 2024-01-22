00f733e2ee094be21cd93169cacc04d7c96907e7
Bryce Berwald <bryce@berwald.io>
2024-01-22 13:48:21 -0800

    Added a box with a character sprite and two radio buttons to select the players gender on the CreateCharacterScene.

--- client/src/components/Game/scenes/CreateCharacterScene.js
+++ client/src/components/Game/scenes/CreateCharacterScene.js
@@ -33,6 +33,17 @@ export default class CreateCharacterScene extends Phaser.Scene {
             console.log("Error Creating Character: Invalid character slot number.");
         }
 
+        // Draw a white 150x150 square in the middle of the screen
+        const square = this.add.graphics();
+        square.fillStyle(0xffffff, 1); // white color
+        square.fillRect(config.width / 2, config.height / 2 - 175, 250, 250);
+
+        // Load and display an image with a specific frame in the middle of the square
+        const frameNumber = 0; // replace with the desired frame number
+        const image = this.add.image(config.width / 2 + 125, config.height / 2 - 50, 'human01', frameNumber); // replace 'yourSpritesheetKey' with the actual spritesheet key
+        image.setOrigin(0.5); // set the origin to the center/ set the origin to the center
+        image.setScale(2); // scale the image by 2
+
         // Add text to the screen to prompt the user to enter a character name
         this.add.text(((config.width/2) - 400), 90, 'Enter Character Name:', { fontSize: '24px', fill: '#fff' });
 
@@ -56,6 +67,14 @@ export default class CreateCharacterScene extends Phaser.Scene {
             };
 
         });
+
+        // Create radio buttons for gender selection
+        const maleRadioButton = this.add.dom(config.width / 2 + 75, config.height / 2 + 100).createFromHTML('<input type="radio" name="gender" id="maleRadio" value="male" checked><label for="maleRadio" style="font-family: Arial, sans-serif; color: #fff;">Male</label>');
+        const femaleRadioButton = this.add.dom(config.width / 2 + 175, config.height / 2 + 100).createFromHTML('<input type="radio" name="gender" id="femaleRadio" value="female"><label for="femaleRadio" style="font-family: Arial, sans-serif; color: #fff;">Female</label>');
+
+        // Set values to created dom elements for styling
+        maleRadioButton.setOrigin(0.5, 0.5);
+        femaleRadioButton.setOrigin(0.5, 0.5);
 
         // Create Character Button
         const createButton = this.add.text(680, config.height - 200, 'Create Character', { fontSize: '24px', fill: '#fff' }).setOrigin(0.5);
